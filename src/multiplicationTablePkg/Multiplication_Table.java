30de5bb67d1a5e4ea2de33702df8297aa4da5960
Pascal Mpawenayo <pm@gmail.com>
2017-01-17 18:25:51 -0500

    src update

---
+++ src/multiplicationTablePkg/Multiplication_Table.java
@@ -0,0 +1,20 @@
+package multiplicationTablePkg;
+
+import java.util.Scanner;
+
+public class Multiplication_Table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int inputNum;
+		int number;
+		
+		Scanner input = new Scanner(System.in);
+		inputNum = input.nextInt();
+		
+		
+
+	}
+
+}
